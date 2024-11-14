b7207aca4f3342a50fbaefbb977bb2a597003570
sameena majeed <sameenamajeed@hamon.in>
2024-11-14 12:31:21 +0530

    add tsconfig file

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+app.get("/", (req, res) => {
+    res.send("All set to goo !!");
+});
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
+module.exports = app;
